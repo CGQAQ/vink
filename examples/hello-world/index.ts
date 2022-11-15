88155865698154a76104d3baf20253fd439903f8
CGQAQ <m.jason.liu@outlook.com>
2022-11-15 16:53:11 +0800

    createVinkApp

--- examples/hello-world/index.ts
+++ examples/hello-world/index.ts
@@ -1,21 +1,6 @@
-import { createApp, createRootNode } from "../../src";
+import { createApp, createVinkApp } from "../../src";
 import HelloWorld from "./hello-world.vue";
 
-const root = createRootNode();
+const root = createVinkApp(HelloWorld);
 
-const app = createApp(HelloWorld);
-
-console.log("--------------------------------");
-console.log("R", root);
-console.log("-------------------------------------------------------");
-
-const r = app.mount(root);
-console.log(
-    "------------------------------------------------------------------------------",
-);
-
-root.addListener(() => {
-    console.log("#current# ", root.children[0].children[0].text);
-});
-
-console.log("R", root);
+console.log(root);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 export * from "./vink-runtime";
 
+import type { Component } from "vue";
 import { renderVinkDomToScreen } from "./renderVinkDomToScreen";
 import { VinkElement } from "./vink-dom";
+import { createApp } from "./vink-runtime";
 function createRootNode() {
     return new VinkElement("root", null);
 }
@@ -11,10 +13,16 @@ export const DEFAULT_WIDTH = 80;
 export type CreateVinkAppOpts = {
     width?: number;
 };
-export function createVinkApp(opts: CreateVinkAppOpts): VinkElement {
+export function createVinkApp(
+    rootEl: Component,
+    opts?: CreateVinkAppOpts,
+): VinkElement {
     const root = createRootNode();
+    const renderer = createApp(rootEl, {});
+    renderer.mount(root);
+
     root.addListener(() => {
-        renderVinkDomToScreen(root, { width: opts.width ?? DEFAULT_WIDTH });
+        renderVinkDomToScreen(root, { width: opts?.width ?? DEFAULT_WIDTH });
     });
 
     return root;
