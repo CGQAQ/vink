066195146f913c3296ef97a1146be79357131909
CGQAQ <m.jason.liu@outlook.com>
2022-11-16 08:53:44 +0800

    update example

--- examples/hello-world/hello-world.vue
+++ examples/hello-world/hello-world.vue
@@ -4,19 +4,20 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watchEffect } from "vue";
+import { ref, onBeforeUnmount, onMounted } from "vue";
 import Text from "../../src/builtin/Text";
 
 const msg = ref("Hello World from vink");
 const counter = ref(0);
 
-watchEffect((onClean) => {
-    const handle = setInterval(() => {
+let handle;
+onMounted(() => {
+    handle = setInterval(() => {
         counter.value++;
     }, 1000);
+});
 
-    onClean(() => {
-        clearInterval(handle);
-    });
+onBeforeUnmount(() => {
+    clearInterval(handle);
 });
 </script>
